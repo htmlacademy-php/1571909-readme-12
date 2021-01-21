e20bea3d544fa77431a4f50597da072f575a18e6
v777999 <55867385+v777999@users.noreply.github.com>
2021-01-21 16:05:44 +0300

    Добавил post__indicators

--- index.php
+++ index.php
@@ -356,6 +356,27 @@ $posts = [
                                     </div>
                                 </a>
                             </div>
+                            <div class="post__indicators">
+                                <div class="post__buttons">
+                                    <a class="post__indicator post__indicator--likes button" href="#" title="Лайк">
+                                        <svg class="post__indicator-icon" width="20" height="17">
+                                            <use xlink:href="#icon-heart"></use>
+                                        </svg>
+                                        <svg class="post__indicator-icon post__indicator-icon--like-active" width="20" height="17">
+                                            <use xlink:href="#icon-heart-active"></use>
+                                        </svg>
+                                        <span>0</span>
+                                        <span class="visually-hidden">количество лайков</span>
+                                    </a>
+                                    <a class="post__indicator post__indicator--comments button" href="#" title="Комментарии">
+                                        <svg class="post__indicator-icon" width="19" height="17">
+                                            <use xlink:href="#icon-comment"></use>
+                                        </svg>
+                                        <span>0</span>
+                                        <span class="visually-hidden">количество комментариев</span>
+                                    </a>
+                                </div>
+                            </div>
                         </footer>
                     </article>
                 <?php endforeach?>
